9aeb87799fd27e8139a1f91cc7791dc3eabf56c2
Brandon Dega <brandondega@gmail.com>
2022-01-31 18:50:07 -0600

    monthly and annual

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
-let answer = parceFloat(window.prompt(weeklyExpenses[0]))
-console.log(answer)
+for(let i = 0; i < weeklyExpensesQuestions.length; i++) {
+    let answer = parceFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+for(let i = 0; i < monthlyExpensesQuestions.length; i++) {
+    let answer = parceFloat(window.prompt(monthlyExpenseQuestions[0]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+for(let i = 0; i < annualExpensesQuestions.length; i++) {
+    let answer = parceFloat(window.prompt(annualExpenseQuestions[0]))
+    annualExpenses = annualExpenses + answer
+}
